97099320c41f29961bf8d026d21b1a52526c2459
Roman Belov <gravirovshik@yandex.ru>
2022-12-07 23:22:26 +0300

    add task 1

---
+++ numberToStrng.js
@@ -0,0 +1,20 @@
+/* We need a function that can transform a number (integer) into a string.*/
+/* Вариант 1 */
+function numberToString(num) {
+    return num.toString();
+  }
+
+/* Вариант 2 */
+function numberToString(num) {
+    // Return a string of the number here!
+    return String(num);
+  }
+
+  /* Вариант 3 */
+  function numberToString(num) {
+    return ''+num;
+  }
+
+/* Вариант 4 */
+  const numberToString = num => `${num}`;
+  
