69442d2b88f9057ca62e6bfc87dc177d7387c3d3
Roman Belov <gravirovshik@yandex.ru>
2022-12-14 13:42:07 +0300

    add task 11

---
+++ 11 - quarterOfYear.js
@@ -0,0 +1,40 @@
+// Решение с использование Switch
+
+const quarterOf = (month, quarter = 4) => {
+	switch (month) {
+		case 1:
+		case 2:
+		case 3:
+			quarter = 1;
+			break;
+		case 4:
+		case 5:
+		case 6:
+			quarter = 2;
+			break;
+		case 7:
+		case 8:
+		case 9:
+			quarter = 3;
+			break;
+	}
+	return quarter;
+};
+
+// Решение с использование if else
+const quarterOf1 = (month) => {
+  if (month <= 3) {
+    return 1
+  } else if (month <= 6) {
+    return 2
+  } else if (month <= 9) {
+    return 3
+  } else if (month <= 12) {
+    return 4
+  }
+  
+}
+
+// Решение приятное
+
+const quarterOf2 = m => Math.ceil(m/3);
