f2bcf23c40318c0badd00d30654fe4c0543be2c9
Roman Belov <gravirovshik@yandex.ru>
2023-01-03 08:13:40 +0300

    add task 28

--- 28 - Find Vowels.js
+++ 28 - Find Vowels.js
@@ -1,11 +1,23 @@
-var str = "hieeelalaooo"; //строка
-var vowels = "aeiou"; //гласные
+function disemvowel(str){
+var vowels = "aeiouAEIOU"; //гласные
 var res = "";
 for (u of str){
 if (vowels.indexOf(u) == -1){
 res = res + u;
 }
- 
 }
 
 console.log(res);
+}
+
+function disemvowel(str) {
+    return str.replace(/[aeiou]/gi, '');
+  }
+
+  function disemvowel(str) {
+    const vowels = 'aeiou';
+    return str
+      .split('')
+      .filter(letter => !vowels.includes(letter.toLowerCase()))
+      .join('');
+  }
