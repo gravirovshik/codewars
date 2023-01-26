e9f5b5db62178a21bbc637ede5c539ed4e664334
Roman Belov <gravirovshik@yandex.ru>
2023-01-26 17:37:34 +0300

    update tesk 48

--- 48 - Train JS round ceil and floor.js
+++ 48 - Train JS round ceil and floor.js
@@ -4,9 +4,20 @@ function roundIt(n){
     console.log(str);
     console.log(str[0].length);
     console.log(str[1].length);
+    if (str[0].length == str[1].length) {
+        return Math.round(n)
+    }
+    else if (str[0].length > str[1].length) {
+        return Math.floor(n)
+    } else return Math.ceil(n);
 
 
 }
 
 
 roundIt(5.755);
+
+function roundIt(n){
+    var [a, b] = n.toString().split('.');
+    return a.length > b.length ? Math.floor(n) : a.length === b.length ? Math.round(n) : Math.ceil(n);
+  }
